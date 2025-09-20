1ff464c577ec3bd5f398be145390fe0b3c6b1358
kroton <kroton@users.noreply.github.com>
2025-09-20 21:15:44 +0900

    2025/09/20 ゼミ

--- LeanBook/Problem61A.lean
+++ LeanBook/Problem61A.lean
@@ -1,7 +1,12 @@
-/- # 問題61A
-
-2分木の葉をリストにして返す関数を定義せよ。
--/
 import LeanBook.Problem61
 
-def leaves {α : Type} (t : BinTree α) : List α := sorry
+def leaves {α : Type} (t : BinTree α) : List α :=
+  match t with
+  | .empty => []
+  | .node v .empty .empty => [v]
+  | .node _ left right =>
+    leaves left ++ leaves right
+
+#guard leaves [tree| 1] = [1]
+#guard leaves [tree| 1 * (2 + 3)] = [2,3]
+#guard leaves [tree| 1 * (2 * (3 + 4) + 5 * (6 + 7))] = [3,4,6,7]
---
+++ LeanBook/Problem62B.lean
@@ -0,0 +1,14 @@
+import LeanBook.Problem62
+
+def atlevel {α : Type} (t : BinTree α) (l : Nat) : List α :=
+  match t, l with
+  | _, 0 => []
+  | .empty, _ => []
+  | .node v _ _, 1 => [v]
+  | .node _ left right, .succ l =>
+    atlevel left l ++ atlevel right l
+
+#guard atlevel [tree| 1] 1 = [1]
+#guard atlevel [tree| 1 * (2 + 3)] 2 = [2, 3]
+#guard atlevel [tree| 1 * (2 * (3 + 4) + 5 * (6 + 7))] 3 = [3,4,6,7]
+#guard atlevel [tree| 1] 0 = []
